7319074e196cfd6107b9bf8e03a124bcd7a53820
Mason Jennings <mason.jennings@alphasights.com>
2016-01-06 13:19:51 -0500

    Props

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 class App extends React.Component {
   render(){
-    return (
-      <div>
-        <h1>Hello World</h1>
-        <b>Bold</b>
-      </div>
-    )
+    return <h1>{this.props.txt}</h1>
   }
 }
 
+App.propTypes = {
+  txt: React.PropTypes.string,
+  cat: React.PropTypes.number.isRequired
+}
 
-export default App
+App.defaultProps = {
+  txt: 'this is the default text'
+}
+
+ReactDOM.render(
+  <App txt="this is the props value" />,
+  document.getElementById('app')
+);
