6a321d306dcf8b07842afce656db2cf35c9e5489
Mason Jennings <mason.jennings@alphasights.com>
2016-01-06 11:31:01 -0500

    Builds first component

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
 import React from 'react';
-class App extends React.Component {
-  render(){
-    return <div>Hello, is it me</div>
-  }
-}
+// class components can have state
+//
+// class App extends React.Component {
+//   render(){
+//     return <h1>Hello Guys</h1>
+//   }
+// }
+//
+
+// stateless function component
+const App = () => <h1>Hello Eggheads</h1>
 
 export default App
